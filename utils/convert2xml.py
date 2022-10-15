f20c600e7c7590ada2db9b36f9d55a00b3440312
travishu <travisergodic1993@gmail.com>
2022-10-15 11:33:04 +0800

    revise convert2xml.py

--- utils/convert2xml.py
+++ utils/convert2xml.py
@@ -1,23 +1,33 @@
 import os
-import glob
+import shutil
 import cv2
-from config import cls_dict
 
 
-def txt2xml(txt_file, xml_file, image_path, image_dir):
+def txt2xml(txt_path, xml_path, image_path, cls_dict):
+    if (not os.path.isfile(txt_path)) and (not os.path.isfile(image_path)):
+        print(f'both {txt_path}, {image_path} do not exist!')
+        return False
+    
+    if not os.path.isfile(txt_path): 
+        print(f'{txt_path} do not exist!')
+        return False
+        
+    if not os.path.isfile(image_path): 
+        print(f'{image_path} do not exist!')
+        return False
+        
     image_height, image_width, _ = cv2.imread(image_path).shape
     image_name = os.path.basename(image_path)
-    image_path = os.path.join(image_dir, image_name)
     
-    with open(txt_file, 'r') as f: 
+    with open(txt_path, 'r') as f: 
         s = f.read().strip()
         if s: 
             lines = s.split('\n')
         else: 
-            print(txt_file)
+            print(txt_path)
             lines = []
 
-    with open(xml_file, 'w') as f:
+    with open(xml_path, 'w') as f:
         f.write('<annotation>\n')
         f.write('\t<folder>data</folder>\n')
         f.write('\t<filename>' + image_name + '</filename>\n')
@@ -32,7 +42,7 @@ def txt2xml(txt_file, xml_file, image_path, image_dir):
         f.write('\t</size>\n')
         f.write('\t<segmented>0</segmented>\n')
 
-        for line in lines: 
+        for line in lines:
             cls, x, y, w, h = line.split(',')
             x_min, y_min = x, y
             x_max, y_max = str(int(x) + int(w)), str(int(y) + int(h))
@@ -53,15 +63,50 @@ def txt2xml(txt_file, xml_file, image_path, image_dir):
 
         # Close the annotation tag once all the objects have been written to the file
         f.write('</annotation>\n')
-        f.close() # Close the file
+    return True
 
-def generate_xml_files(txt_paths, xml_dir, image_dir): 
-    for txt_path in txt_paths:
-        xml_path = os.path.join(xml_dir, os.path.basename(txt_path).replace('.txt', '.xml')) 
-        image_path = os.path.join(image_dir, os.path.basename(txt_path).replace('.txt', '.png'))
-        txt2xml(txt_path, xml_path, image_path, image_dir)
+
+def create_VOCdevkit(image_dir, target_dir, image_suffix, target_suffix, cls_dict): 
+    # create directories
+    os.mkdir('./VOCdevkit/')
+    os.mkdir('./VOCdevkit/VOC2007/')
+    os.mkdir('./VOCdevkit/VOC2007/Annotations/')
+    os.mkdir('./VOCdevkit/VOC2007/JPEGImages/')
+    os.mkdir('./VOCdevkit/VOC2007/ImageSets/')
+    os.mkdir('./VOCdevkit/VOC2007/ImageSets/Main/')
+    
+    image_names = [] 
+    
+    # cp images & xml
+    for image_file in [file for file in os.listdir(image_dir) if file.endswith(image_suffix)]: 
+        if target_suffix == '.txt': 
+            res = txt2xml(
+                os.path.join(target_dir, image_file.split('.')[0] + target_suffix), 
+                os.path.join('./VOCdevkit/VOC2007/Annotations/', image_file.split('.')[0] + '.xml'), 
+                os.path.join(image_dir, image_file),
+                cls_dict
+            )        
+        
+        else: 
+            res = os.path.isfile(os.path.join(target_dir, image_file.split('.')[0] + target_suffix))
+ 
+        if res:
+            shutil.copyfile(
+                os.path.join(image_dir, image_file), 
+                os.path.join('./VOCdevkit/VOC2007/JPEGImages/', image_file)
+            )
+            
+        image_names.append(image_file.split('.')[0])
+            
+    with open('./VOCdevkit/VOC2007/ImageSets/Main/trainval.txt', 'w') as f:
+        f.write('\n'.join(image_names))
 
 
 if __name__ == '__main__': 
-    txt_paths = glob.glob('/content/train/*.txt')
-    generate_xml_files(txt_paths, '/content/xml_annotation/', '/content/train/')
+    cls_dict = {
+        '0': 'car',
+        '1' : 'hov',
+        '2' : 'person',
+        '3' : 'motorcycle',
+    }
+    create_VOCdevkit('/content/train', '/content/train', '.png', '.txt', cls_dict)

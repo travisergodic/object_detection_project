4a8104a51b863b4d03e1c9922c16cf67cd5fbf10
travishu <travisergodic1993@gmail.com>
2022-10-17 22:28:37 +0800

    add custom_demo.py script

--- utils/demo.py
+++ utils/demo.py
@@ -1,6 +1,6 @@
 import os
 import cv2
-from config import color_dict
+from .config import color_dict
 
 def demo_result(img_path, txt_file, save_dir): 
     img = cv2.imread(img_path)
@@ -18,3 +18,15 @@ def demo_result(img_path, txt_file, save_dir):
         cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color_dict[cls], 2)
 
     cv2.imwrite(os.path.join(save_dir, 'demo_' + os.path.basename(img_path)))
+
+
+def txt_to_csv(txt_folder, csv_path):
+    for txt_file in [file for file in os.listdir(txt_folder) if file.endswith('.txt')]: 
+        text = ''
+        with open(os.path.join(txt_folder, txt_file), 'r') as f: 
+            for str in f.readlines(): 
+                text += txt_file.split('.')[0] + ',' + str
+        text = text.strip()
+    
+    with open(csv_path, 'w') as f:
+        f.write(text)
